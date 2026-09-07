1bf23ab3b373013181897ed2c844252443091b93
Dietmar Planitzer <31997608+dplanitzer@users.noreply.github.com>
2026-09-06 17:24:43 -0700

    Code reorganization

--- Cinemadora/ui/view/CreditsView.swift
+++ Cinemadora/ui/view/CreditsView.swift
@@ -8,10 +8,7 @@
 import SwiftUI
 
 struct CreditsView<Target: NavigationTarget>: View {
-    
-    private let PROFILE_WIDTH = 72.0
-    private let PROFILE_HEIGHT = 110.0
-    
+        
     private var namespace: Namespace.ID
 
     private let people: [any Person]
@@ -32,8 +29,6 @@ struct CreditsView<Target: NavigationTarget>: View {
                 ForEach(people, id: \.id) { person in
                     NavigationLink(value: Target(id: person.id)) {
                         ProfileView(person, imageResolver)
-                            .frame(width: PROFILE_WIDTH, height: PROFILE_HEIGHT)
-                            .clipShape(.rect(cornerRadius: 10.0))
                             .matchedTransitionSource(id: person.id, in: namespace)
                     }
                     .buttonStyle(.plain)
@@ -42,69 +37,6 @@ struct CreditsView<Target: NavigationTarget>: View {
             .scrollTargetLayout()
         }
         .scrollTargetBehavior(.viewAligned)
-    }
-}
-
-
-private struct ProfileView: View {
-    
-    private var person: any Person
-    private let imageResolver: (any Person) -> ImageLocator
-    
-    
-    init(_ person: any Person, _ imageResolver: @escaping (any Person) -> ImageLocator) {
-        self.person = person
-        self.imageResolver = imageResolver
-    }
-    
-    var body: some View {
-        
-        AsyncImageView(imageResolver(person), size: .middle) { state in
-            switch state {
-            case .loaded(let image):
-                Image(uiImage: image)
-                    .resizable()
-                    .scaledToFill()
-                            
-            case .failed, .fallback:
-                ZStack {
-                    Color(white: 0.22)
-                                
-                    Circle()
-                        .stroke(Color(white: 0.75), lineWidth: 2)
-                        .frame(width: 50, height: 50)
-                        .overlay {
-                            Text(getInitials(person.name))
-                                .font(.system(size: 16, weight: .bold, design: .rounded))
-                                .foregroundColor(.white)
-                        }
-                }
-                            
-            default:
-                Color(white: 0.22)
-            }
-        }
-    }
-    
-    private func getInitials(_ fullName: String) -> String {
-        let components = fullName.split(separator: " ").map { $0 }
-        
-        guard let firstComponent = components.first,
-              let firstInitial = firstComponent.first else {
-            // this shouldn't happen in real life
-            return ""
-        }
-        
-        if components.count == 1 {
-            return firstInitial.uppercased()
-        }
-        
-        if let lastComponent = components.last,
-           let lastInitial = lastComponent.first {
-            return "\(firstInitial)\(lastInitial)".uppercased()
-        }
-        
-        return firstInitial.uppercased()
     }
 }
 
---
+++ Cinemadora/ui/view/ProfileView.swift
@@ -0,0 +1,101 @@
+//
+//  ProfileView.swift
+//  Cinemadora
+//
+//  Created by Dietmar Planitzer on 9/6/26.
+//
+
+import SwiftUI
+
+struct ProfileView: View {
+    
+    private let PROFILE_WIDTH = 72.0
+    private let PROFILE_HEIGHT = 110.0
+
+    private var person: any Person
+    private let imageResolver: (any Person) -> ImageLocator
+    
+    
+    init(_ person: any Person, _ imageResolver: @escaping (any Person) -> ImageLocator) {
+        
+        self.person = person
+        self.imageResolver = imageResolver
+    }
+    
+    var body: some View {
+        
+        AsyncImageView(imageResolver(person), size: .middle) { state in
+            switch state {
+            case .loaded(let image):
+                Image(uiImage: image)
+                    .resizable()
+                    .scaledToFill()
+                            
+            case .failed, .fallback:
+                ZStack {
+                    Color(white: 0.22)
+                                
+                    Circle()
+                        .stroke(Color(white: 0.75), lineWidth: 2)
+                        .frame(width: 50, height: 50)
+                        .overlay {
+                            Text(getInitials(person.name))
+                                .font(.system(size: 16, weight: .bold, design: .rounded))
+                                .foregroundColor(.white)
+                        }
+                }
+                            
+            default:
+                Color(white: 0.22)
+            }
+        }
+        .frame(width: PROFILE_WIDTH, height: PROFILE_HEIGHT)
+        .clipShape(.rect(cornerRadius: 10.0))
+    }
+    
+    private func getInitials(_ fullName: String) -> String {
+        
+        let components = fullName.split(separator: " ").map { $0 }
+        
+        guard let firstComponent = components.first,
+              let firstInitial = firstComponent.first else {
+            // this shouldn't happen in real life
+            return ""
+        }
+        
+        if components.count == 1 {
+            return firstInitial.uppercased()
+        }
+        
+        if let lastComponent = components.last,
+           let lastInitial = lastComponent.first {
+            return "\(firstInitial)\(lastInitial)".uppercased()
+        }
+        
+        return firstInitial.uppercased()
+    }
+}
+
+
+#Preview {
+    @State @Previewable var person: (any Person)? = nil
+    let movieRep = MockMovieRepository()
+    let imageRep = MockImageRepository()
+
+    Group {
+        if let person = person {
+            ProfileView(
+                person,
+                { (person: any Person) in
+                    return ImageLocator(imageRep, person.profilePath, .profile)
+                }
+            )
+        } else {
+            ProgressView()
+        }
+    }
+    .preferredColorScheme(.dark)
+    .task {
+        person = try! await movieRep.fetchCredits(for: 550).cast.first
+    }
+}
